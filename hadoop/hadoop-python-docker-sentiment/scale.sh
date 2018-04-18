1a1b910fbd666d45cc88788e4e6cc745fc21cc33
root <root@localhost.localdomain>
2018-04-17 22:11:35 -0400

    finish testing and modifying passing number of workers to run hadoop clusters

--- hadoop/hadoop-python-docker-sentiment/scale.sh
+++ hadoop/hadoop-python-docker-sentiment/scale.sh
@@ -7,13 +7,13 @@ fi
 cp ./docker-compose-template.yml ./docker-compose.yml
 for i in $(seq 2 $1)
 do
-echo  "  worker"$i":\n\
-	build: ./hadoop-worker\n\
-	container_name: \"worker"$i"\"\n\
-	ports:\n\
-	  - \"$((9900+$i)):9864\"\n\
-	  - \"$((8040+$i)):8042\""\
-	>>./docker-compose.yml
+echo -e "  worker"$i":\n\
+    build: ./hadoop-worker\n\
+    container_name: \"worker"$i"\"\n\
+    ports:\n\
+      - \"$((9900+$i)):9864\"\n\
+      - \"$((8040+$i)):8042\""\
+>>./docker-compose.yml
 done
 echo "docker-compose.yml created with #worker "$1
 exit 0
