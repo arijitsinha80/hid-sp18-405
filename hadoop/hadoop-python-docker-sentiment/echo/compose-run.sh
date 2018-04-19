59fa363bbdf6775dca0905ecd3a81888f24784d8
Min Chen <min.chen.1015@gmail.com>
2018-04-19 01:34:29 -0400

    modify the syntax of shell script

--- hadoop/hadoop-python-docker-sentiment/echo/compose-run.sh
+++ hadoop/hadoop-python-docker-sentiment/echo/compose-run.sh
@@ -13,9 +13,9 @@ echo "running the sentiment analysis on movie reviews..."
 docker exec master /etc/runall.sh
 
 echo "getting the results..."
-rm -rf $(DESTDIR)
-mkdir $(DESTDIR)
-docker cp master:/cloudmesh/python/output_pos_tagged ./$(DESTDIR)
-docker cp master:/cloudmesh/python/output_neg_tagged ./$(DESTDIR)
-docker cp master:/cloudmesh/python/log.txt ./$(DESTDIR)
-cp docker-compose.yml ./$(DESTDIR)
+rm -rf $DESTDIR
+mkdir $DESTDIR
+docker cp master:/cloudmesh/python/output_pos_tagged ./$DESTDIR
+docker cp master:/cloudmesh/python/output_neg_tagged ./$DESTDIR
+docker cp master:/cloudmesh/python/log.txt ./$DESTDIR
+cp docker-compose.yml ./$DESTDIR
