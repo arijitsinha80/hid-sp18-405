35aea94f5c5642681f84b0058345a2d63d86964b
Min Chen <min.chen.1015@gmail.com>
2018-04-21 00:43:59 -0400

    remove previous test folder

--- hadoop/testSwarm/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf Results*
-rm -rf benchmark-full
-rm -rf benchmark-pseudo
-rm docker-compose.yml
--- hadoop/testSwarm/hadoop-master/python/runall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /cloudmesh/python
-(time ./runPythonMapReduce.sh) 2>&1 | tee -a /cloudmesh/python/log.txt
