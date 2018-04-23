1f60a9d89f8ba86e80542d029ee83a3bdc783178
Min Chen <min.chen.1015@gmail.com>
2018-04-23 01:40:33 -0400

    try swarm

--- hadoop/hadoop-python-docker-sentiment/hadoop-master/bootstrap.sh
+++ hadoop/hadoop-python-docker-sentiment/hadoop-master/bootstrap.sh
@@ -20,8 +20,10 @@ nohup $HADOOP_PREFIX/bin/yarn resourcemanager &
 nohup $HADOOP_PREFIX/bin/yarn timelineserver &
 nohup $HADOOP_PREFIX/bin/mapred historyserver &
 
+sleep 30
+/cloudmesh/python/runPythonMapReduce.sh
+
 if [[ $1 == "-d" ]]; then
-    /cloudmesh/python/runPythonMapReduce.sh
     while true; do sleep 1000; done
 fi
 
