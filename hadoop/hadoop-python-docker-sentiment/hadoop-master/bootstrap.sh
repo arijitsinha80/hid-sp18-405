6f126e1d1e207984feb6ab3b57f0fded7fd3fd29
Min Chen <min.chen.1015@gmail.com>
2018-04-24 01:15:06 -0400

    cleanup for docker swarm

--- hadoop/hadoop-python-docker-sentiment/hadoop-master/bootstrap.sh
+++ hadoop/hadoop-python-docker-sentiment/hadoop-master/bootstrap.sh
@@ -39,10 +39,13 @@ fi
 
 if [[ $1 == "-benchmark" ]]; then
     sleep 30
+    export PATH=$PATH:/$HADOOP_PREFIX/bin
     for i in $(seq 1 $2)
-    export PATH=$PATH:/$HADOOP_PREFIX/bin
-    (time /cloudmesh/python/runPythonMapReduce.sh) 2>&1 | tee -a /cloudmesh/python/log.txt
-    export PATH=$PATH:/$HADOOP_PREFIX/bin
-    hadoop fs -put /cloudmesh/python/log.txt /
+        hadoop fs -rm -R /nlp
+        (time /cloudmesh/python/runPythonMapReduce.sh) 2>&1 | tee -a /cloudmesh/python/log.txt
+        tail -3 /cloudmesh/python/log.txt |head -1>>./cloudmesh/python/$3_worker.txt
+        hadoop fs -rm /$3_worker.txt
+        hadoop fs -put /cloudmesh/python/$3_worker.txt /
+    done
     while true; do sleep 1000; done
 fi
--- hadoop/hadoop-python-docker-sentiment/swarm-run.sh
+++ hadoop/hadoop-python-docker-sentiment/swarm-run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+worker=1
+else worker=$1
+fi
 
 echo "starting the containers in swarm mode"
 docker stack deploy --compose-file docker-swarm.yml hadoop-sentiment
+echo "scale up the service to $worker worker"
+docker service scale hadoop-sentiment_worker=$worker
+
 echo "running the sentiment analysis on movie reviews at backend..."
-
-echo "pausing for 20s"
-sleep 20
-echo "Continuing ...."
-
 echo "getting physical node that runs master"
 nodeID=$(docker stack ps  -f "name=hadoop-sentiment_master.1" --format "{{.Node}}" hadoop-sentiment)
 echo $nodeID
