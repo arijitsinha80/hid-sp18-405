e84feeec932a517ec5bc788ac3951d682afc83e2
Min Chen <min.chen.1015@gmail.com>
2018-04-24 23:36:39 -0400

    add temp run for the benchmanr

--- hadoop/hadoop-python-docker-sentiment/tempRun.sh
+++ hadoop/hadoop-python-docker-sentiment/tempRun.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
- ./benchmarking-swarm.sh 10 9
- ./benchmarking-swarm.sh 10 8
+ ./benchmarking-swarm.sh 10 7
+ ./benchmarking-swarm.sh 10 6
+ ./benchmarking-swarm.sh 10 5
+ ./benchmarking-swarm.sh 10 4
+ ./benchmarking-swarm.sh 10 3
+ ./benchmarking-swarm.sh 10 2
+ ./benchmarking-swarm.sh 10 1
