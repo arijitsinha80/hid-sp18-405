ff267519de16aa64cdba7072e5817007fc9429cb
Min Chen <min.chen.1015@gmail.com>
2018-04-26 00:30:25 -0400

    next step benchmarking scripts

---
+++ hadoop/hadoop-python-docker-sentiment/benchmarking-full_temp.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo 1>&2 Usage: [# of iteration][# of workers]
+    echo "e.g. 5"
+    exit -1
+fi
+
+worker=$2
+
+DESTDIR=benchmark-full
+mkdir -p $DESTDIR
+
+docker network rm hadoop-sentiment
+docker network create hadoop-sentiment
+
+echo "Build the image"
+docker build -t minchen57/hadoop-docker-python-sentiment-compose-base:latest hadoop-base
+docker build -t minchen57/hadoop-docker-python-sentiment-compose-master:latest hadoop-master
+docker build -t minchen57/hadoop-docker-python-sentiment-compose-worker:latest hadoop-worker
+
+for i in $(seq 1 $1)
+do
+echo "Worker# =" $2 ", Iter :" $i
+
+echo "starting the containers..."
+docker-compose scale master1=1 worker=$((worker))
+
+echo "pause for 20 seconds"
+sleep 20
+echo "Run the container with sentiment analysis on hadoop"
+docker exec master1 /etc/runall.sh
+
+echo "Get the results"
+#docker cp pseudo-hadoop:/cloudmesh/python/output_pos_tagged ./$DESTDIR/output_pos_tagged_$TAG
+#docker cp pseudo-hadoop:/cloudmesh/python/output_neg_tagged ./$DESTDIR/output_neg_tagged_$TAG
+docker cp master1:/cloudmesh/python/log.txt ./$DESTDIR/temp.txt
+
+echo "Write the result"
+tail -3 ./$DESTDIR/temp.txt |head -1>>./$DESTDIR/$2_worker.txt
+rm ./$DESTDIR/temp.txt
+echo "Stop the container"
+docker-compose down
+done
+
+docker network rm hadoop-sentiment
+
+
+
+
---
+++ hadoop/hadoop-python-docker-sentiment/tempRun2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./benchmarking-swarm.sh 6 15
+./benchmarking-swarm.sh 4 20
+./benchmarking-swarm.sh 5 25
+./benchmarking-swarm.sh 9 30
+./benchmarking-swarm.sh 10 35
+./benchmarking-swarm.sh 9 40
