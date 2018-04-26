1d91beb5e81bc39b3a58b10b9ae3987be04264c2
Min Chen <min.chen.1015@gmail.com>
2018-04-26 00:34:58 -0400

    revert changes in yml file

--- hadoop/hadoop-python-docker-sentiment/tempRun.sh
+++ hadoop/hadoop-python-docker-sentiment/tempRun.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
- ./benchmarking-full_temp.sh 10 9
- ./benchmarking-full_temp.sh 10 10
-./benchmarking-full_temp.sh 10 15
-./benchmarking-full_temp.sh 10 20
-./benchmarking-full_temp.sh 10 25
-./benchmarking-full_temp.sh 10 30
-./benchmarking-full_temp.sh 10 35
-./benchmarking-full_temp.sh 10 40
+
+./benchmarking-swarm.sh 6 15
+./benchmarking-swarm.sh 4 20
+./benchmarking-swarm.sh 5 25
+./benchmarking-swarm.sh 9 30
+./benchmarking-swarm.sh 10 35
+./benchmarking-swarm.sh 9 40
+./benchmarking-full.sh 10 9
+./benchmarking-full.sh 10 10
+./benchmarking-full.sh 10 15
+./benchmarking-full.sh 10 20
+./benchmarking-full.sh 10 25
+./benchmarking-full.sh 10 30
+./benchmarking-full.sh 10 35
+./benchmarking-full.sh 10 40
