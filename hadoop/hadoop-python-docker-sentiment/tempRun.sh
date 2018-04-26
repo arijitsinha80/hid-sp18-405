a90bb0390c77473ff11acf571478bac4702ee561
Min Chen <min.chen.1015@gmail.com>
2018-04-26 07:33:15 -0400

    run benchmarding test with auto script

--- hadoop/hadoop-python-docker-sentiment/tempRun.sh
+++ hadoop/hadoop-python-docker-sentiment/tempRun.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 
-./benchmarking-swarm.sh 6 15
-./benchmarking-swarm.sh 4 20
-./benchmarking-swarm.sh 5 25
-./benchmarking-swarm.sh 9 30
-./benchmarking-swarm.sh 10 35
 ./benchmarking-swarm.sh 9 40
 ./benchmarking-full.sh 10 9
 ./benchmarking-full.sh 10 10
