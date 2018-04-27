ab2c973af466a811f77fb8307d7c2906eabb1f17
Min Chen <min.chen.1015@gmail.com>
2018-04-27 13:52:03 -0400

    cleanup benchmark

---
+++ hadoop/hadoop-python-docker-sentiment/pull.sh
@@ -0,0 +1,5 @@
+docker pull minchen57/hadoop-docker-python-sentiment-compose-base:latest
+docker pull minchen57/hadoop-docker-python-sentiment-compose-master:latest
+docker pull minchen57/hadoop-docker-python-sentiment-compose-worker:latest
+
+
